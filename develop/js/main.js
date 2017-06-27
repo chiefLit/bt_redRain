5fdd792ef5d4503349a87438974bad743e9a888e
lit <lt@cheok.com>
2017-06-27 16:55:47 +0800

    1

---
+++ develop/js/main.js
@@ -0,0 +1,155 @@
+$(function () {
+    var util = {
+        //向已知dom后添加新元素
+        appendChildDom: function (domChild, domParent) {
+            domParent.append(domChild);
+
+        },
+        //创建空的canvas
+        createCanvas: function (width, height, id, parentDOM) {
+            var canvas = $('<canvas></canvas>');
+            canvas.attr({
+                id: id,
+                width: width,
+                height: height
+            });
+            this.appendChildDom(canvas, parentDOM);
+            return canvas;
+        },
+        //随机数创建
+        random: function(start, end) {
+            return Math.floor(Math.random() * (end - start + 1)) + start;
+        }
+    };
+
+    var RedRain = function (name, options, callback) {
+        this.WIDTH = options.WIDTH || $(window).width();
+        this.HEIGHT = options.HEIGHT || $(window).height();
+        // 创建画布
+        var canvas = util.createCanvas(this.WIDTH, this.HEIGHT, "canvas", $('body'))[0];
+        this.ctx = canvas.getContext('2d');
+        //背景图片 红包图片
+        this.image_bg = options.image_bg;
+        this.imgage_rd = options.imgage_rd;
+        this.rd_width = options.rd_width;
+        this.rd_height = options.rd_height;
+        //几率
+        this.odds = options.odds;
+        //红包列表
+        this.rds = [];
+    }
+
+    RedRain.prototype = {
+        //初始化画布
+        init: function() {
+            var _this = this;
+            _this.setBg();
+        },
+
+        //设置背景
+        setBg: function(){
+            var _this = this;
+            var img = new Image();
+            img.src = _this.image_bg;
+            img.onload = function() {
+                _this.ctx.drawImage(img, 0, 0, _this.WIDTH, _this.HEIGHT);
+                _this.startGame();
+            };
+        },
+
+        //创建红包
+        createRed: function() {
+            var _this = this;
+            var img = new Image();
+            var oImg = _this.imgage_rd[util.random(0,_this.imgage_rd.length - 1)];
+            img.src = oImg.url;
+            var dir = 0;
+            var x = util.random(10, _this.WIDTH - oImg.width - 10);
+            img.onload = function() {
+                var ctx = _this.ctx;
+                // ctx.drawImage(img, x, 0, oImg.width, oImg.height);
+                var downTimer = setInterval(function(){
+                    ctx.clearRect(x,0, oImg.width, oImg.height);//清除上一次的痕迹
+                    ctx.beginPath();
+                    ctx.drawImage(img, x, dir, oImg.width, oImg.height);
+                    ctx.closePath();
+                    // ctx.fill();
+                    if (dir > _this.HEIGHT) {
+                        clearInterval(downTimer);
+                    } else {
+                        dir += 10
+                    }
+                }, 70)
+                // ctx.rotate(20*Math.PI/180);
+            };
+        },
+
+        //开始游戏
+        startGame: function(){
+            var _this = this;
+            var sum = 0;
+            var timer = setInterval(function(){
+                var nNandom = Math.random();
+                sum ++;
+                if (nNandom < _this.odds) {
+                    _this.createRed();
+                }
+                if (sum >= 100) {
+                    clearInterval(timer);
+                    _this.endGame()
+                }
+            }, 100);
+        },
+
+        // 动画
+        animate: function(){
+        },
+
+        //结束游戏
+        endGame: function() {
+
+        },
+    }
+
+    // 红包
+    // 1.生成的单位时间 100ms
+    // 2.生成红包的几率
+    // 3.红包的个数
+    // Q:红包下降速率快，红包量大如何控制
+    // Q:红包量大的概念是页面展示出来的整屏速度吗？
+    // A:是的，如果要求数目大的话我要加大生成红包的数目 
+    
+    //红包雨参数
+    var options = {
+        WIDTH: $(window).width(),
+        HEIGHT: $(window).height(),
+        //背景图片 红包图片
+        image_bg: "img/redpacket_background@2x.png",
+        imgage_rd: [{
+            url: "img/redpacket_person1@2x.png",
+            width: 58,
+            height: 102
+        },{
+            url: "img/redpacket_person2@2x.png",
+            width: 84,
+            height: 71
+        },{
+            url: "img/redpacket_person3@2x.png",
+            width: 72,
+            height: 71
+        },{
+            url: "img/redpacket_person4@2x.png",
+            width: 92,
+            height: 71
+        }],
+        rd_width: 50,
+        rd_height: 50,
+        //单个红包出现记录
+        odds: 0.6
+    } 
+
+
+    window.RedRain = RedRain;
+    var rr = new RedRain("canvas", options);
+    rr.init();
+});
